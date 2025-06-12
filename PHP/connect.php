d640c9d954359618c84a4a0175207a0eeceb29f2
Georgia Kapitsaki <>
2025-06-12 14:21:09 +0300

    Adding license file (GPL-3.0)

--- PHP/connect.php
+++ PHP/connect.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli("localhost", "root","","ade");
+$conn = new mysqli("localhost", "root","","fosschain");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
